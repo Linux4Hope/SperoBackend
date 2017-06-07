17cc8ff162ae929639146d6ccfb8806597586f78
Narith <narithchoeun@hotmail.com>
2017-06-06 17:39:09 -0700

    config troubles

---
+++ src/test/java/ApplicationTests/ApplicationTest.java
@@ -0,0 +1,17 @@
+
+package src.main.java.app.Application;
+package ApplicationTests;
+import org.junit.Test;
+
+public class ApplicationTest {
+    @Test
+    public void getHelloTest() {
+        // Arrange
+        Application app = new Application();
+
+        // Act
+        app.getHello();
+
+        // Assert
+    }
+}
