b4a52efb25bb1aac5a8113c14aa4d370d8fc1e8f
nguym077 <itsmariann@gmail.com>
2017-06-07 02:25:42 -0700

    reorganized files so that it's easier to read.

--- src/queries/spero_ddl.sql
+++ src/queries/spero_ddl.sql
@@ -1,27 +1,3 @@
---Look up computers donated by one individual.
---Look up specific computer(s) donated to a person or entity.
---Look up all computers signed off by one officer/branch.
---Look up computers where recycled status is true.
---Look up specific computer by serial number.
---Look up specific computer by processor, ram, storage.
---Look up all computer by specific make or model.
---Look up all computers by a specific part installed in it.
---Look up all parts available.
---Look up all parts by part type (storage, ram, optical drive, etc).
---Look up all computers donated by specific date or date range.
-
---Find all donations.
---Find all donations by date (specific or range), donee, or signed off by officer.
---Find all expenses.
---Find all expenses by date (specific or range), paid to whom, or signed off by officer.
---Calculate all donations by date (specific or range).
---Calculate all expenses by date (specific or range).
-
---Find all submissions for specific email address.
---Find all donation application (specific date, range of dates, email address).
---Find all applying applications (specific date, range of dates, email address).
-
-
 CREATE TABLE IF NOT EXISTS Users (
     userID      VARCHAR(36) NOT NULL PRIMARY KEY,
     firstName   VARCHAR(25),
