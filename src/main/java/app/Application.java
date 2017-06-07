b93945be3f4b40850f3aedeb73790911cabe0fff
nguym077 <itsmariann@gmail.com>
2017-06-06 19:15:02 -0700

    created queries mysql file. initial setup

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -6,7 +6,6 @@ import static spark.Spark.*;
 
 public class Application {
     public static void main(String[] args) {
-
         get("/test", (req, res) -> "make sure you 'import' the project. NOT open." +
             " and then change the module name under project settings.");
         get("/dependencies", (request, response) -> "Finally got the Maven dependencies to work!");
