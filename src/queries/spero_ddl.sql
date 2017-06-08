64395b00e075788fbe5c2b1eef8ca257d08a7151
nguym077 <itsmariann@gmail.com>
2017-06-07 23:56:33 -0700

    renamed attributes to make it easier to read when I join tables

--- src/queries/spero_ddl.sql
+++ src/queries/spero_ddl.sql
@@ -1,4 +1,19 @@
+DROP TABLE FP_Comp;
+DROP TABLE FP_PartComp;
+DROP TABLE FP_Parts;
 DROP TABLE FinalProduct;
+DROP TABLE Monetary;
+DROP TABLE Computer;
+DROP TABLE Part;
+DROP TABLE Request;
+DROP TABLE Donation;
+DROP TABLE Application;
+DROP TABLE Expenses;
+DROP TABLE Income;
+DROP TABLE Branch;
+DROP TABLE Officer;
+DROP TABLE Guest;
+DROP TABLE Users;
 
 
 CREATE TABLE IF NOT EXISTS Users (
@@ -88,42 +103,42 @@ CREATE TABLE IF NOT EXISTS Application (
 );
 
 CREATE TABLE IF NOT EXISTS Donation (
-    officerID       VARCHAR(36) NOT NULL,
-    userID          VARCHAR(36) NOT NULL,
+    dOfficerID       VARCHAR(36) NOT NULL,
+    donatedBy       VARCHAR(36) NOT NULL,
     bLocation       VARCHAR(50) NOT NULL,
     aDate           DATE NOT NULL,
     dQuestion       TEXT,
-    CONSTRAINT PK_Donation PRIMARY KEY (officerID, userID, bLocation, aDate),
-    CONSTRAINT FK_ApplicationDonation FOREIGN KEY (officerID, userID, bLocation, aDate)
+    CONSTRAINT PK_Donation PRIMARY KEY (dOfficerID, donatedBy, bLocation, aDate),
+    CONSTRAINT FK_ApplicationDonation FOREIGN KEY (dOfficerID, donatedBy, bLocation, aDate)
         REFERENCES Application(officerID, userID, bLocation, aDate)
 );
 
 CREATE TABLE IF NOT EXISTS Request (
-    officerID       VARCHAR(36) NOT NULL,
-    userID          VARCHAR(36) NOT NULL,
+    rOfficerID      VARCHAR(36) NOT NULL,
+    requestedBy     VARCHAR(36) NOT NULL,
     bLocation       VARCHAR(50) NOT NULL,
     aDate           DATE NOT NULL,
     rQuestion       TEXT,
-    CONSTRAINT PK_Request PRIMARY KEY (officerID, userID, bLocation, aDate),
-    CONSTRAINT FK_ApplicationRequest FOREIGN KEY (officerID, userID, bLocation, aDate)
+    CONSTRAINT PK_Request PRIMARY KEY (rOfficerID, requestedBy, bLocation, aDate),
+    CONSTRAINT FK_ApplicationRequest FOREIGN KEY (rOfficerID, requestedBy, bLocation, aDate)
         REFERENCES Application(officerID, userID, bLocation, aDate)
 );
 
 CREATE TABLE IF NOT EXISTS Part (
-    officerID       VARCHAR(36),
-    userID          VARCHAR(36),
+    dOfficerID       VARCHAR(36),
+    donatedBy       VARCHAR(36),
     bLocation       VARCHAR(50),
     aDate           DATE,
     pSerialNumber   VARCHAR(50) NOT NULL PRIMARY KEY,
     pType           VARCHAR(50),
     notes           TEXT,
-    CONSTRAINT FK_DonationPart FOREIGN KEY (officerID, userID, bLocation, aDate)
-        REFERENCES Donation(officerID, userID, bLocation, aDate)
+    CONSTRAINT FK_DonationPart FOREIGN KEY (dOfficerID, donatedBy, bLocation, aDate)
+        REFERENCES Donation(dOfficerID, donatedBy, bLocation, aDate)
 );
 
 CREATE TABLE IF NOT EXISTS Computer (
-    officerID           VARCHAR(36),
-    userID              VARCHAR(36),
+    dOfficerID          VARCHAR(36),
+    donatedBy           VARCHAR(36),
     bLocation           VARCHAR(50),
     aDate               DATE,
     cSerialNumber       VARCHAR(50) NOT NULL PRIMARY KEY,
@@ -135,56 +150,56 @@ CREATE TABLE IF NOT EXISTS Computer (
     conditionStatus     VARCHAR(50),
     recycledStatus      VARCHAR(50),
     notes               TEXT,
-    CONSTRAINT FK_DonationComputer FOREIGN KEY (officerID, userID, bLocation, aDate)
-        REFERENCES Donation(officerID, userID, bLocation, aDate)
+    CONSTRAINT FK_DonationComputer FOREIGN KEY (dOfficerID, donatedBy, bLocation, aDate)
+        REFERENCES Donation(dOfficerID, donatedBy, bLocation, aDate)
 );
 
 CREATE TABLE IF NOT EXISTS Monetary (
-    officerID           VARCHAR(36),
-    userID              VARCHAR(36) NOT NULL,
+    dOfficerID          VARCHAR(36),
+    donatedBy           VARCHAR(36) NOT NULL,
     bLocation           VARCHAR(50) NOT NULL,
     aDate               DATE NOT NULL,
     amount              DOUBLE NOT NULL,
-    CONSTRAINT PK_Monetary PRIMARY KEY (userID, bLocation, aDate, amount),
-    CONSTRAINT FK_DonationMonetary FOREIGN KEY (officerID, userID, bLocation, aDate)
-        REFERENCES Donation(officerID, userID, bLocation, aDate)
+    CONSTRAINT PK_Monetary PRIMARY KEY (donatedBy, bLocation, aDate, amount),
+    CONSTRAINT FK_DonationMonetary FOREIGN KEY (dOfficerID, donatedBy, bLocation, aDate)
+        REFERENCES Donation(dOfficerID, donatedBy, bLocation, aDate)
 );
 
 CREATE TABLE IF NOT EXISTS FinalProduct (
-    officerID       VARCHAR(36) NOT NULL,
-    userID          VARCHAR(36) NOT NULL,
+    rOfficerID      VARCHAR(36) NOT NULL,
+    requestedBy     VARCHAR(36) NOT NULL,
     bLocation       VARCHAR(50) NOT NULL,
     aDate           DATE,
     dateDonated     DATE NOT NULL,
     operatingSystem VARCHAR(50),
-    CONSTRAINT PK_FinalProduct PRIMARY KEY (officerID, userID, bLocation, dateDonated),
-    CONSTRAINT FK_RequestFinalProduct FOREIGN KEY (officerID, userID, bLocation, aDate)
-        REFERENCES Request(officerID, userID, bLocation, aDate)
+    CONSTRAINT PK_FinalProduct PRIMARY KEY (rOfficerID, requestedBy, bLocation, dateDonated),
+    CONSTRAINT FK_RequestFinalProduct FOREIGN KEY (rOfficerID, requestedBy, bLocation, aDate)
+        REFERENCES Request(rOfficerID, requestedBy, bLocation, aDate)
 );
 
 CREATE TABLE IF NOT EXISTS FP_Parts (
-    officerID       VARCHAR(36) NOT NULL,
-    userID          VARCHAR(36) NOT NULL,
+    rOfficerID      VARCHAR(36) NOT NULL,
+    requestedBy     VARCHAR(36) NOT NULL,
     bLocation       VARCHAR(50),
     dateDonated     DATE NOT NULL,
     pSerialNumber   VARCHAR(50) NOT NULL,
-    CONSTRAINT PK_fpParts PRIMARY KEY (officerID, userID, dateDonated, pSerialNumber),
-    CONSTRAINT FK_FinalProductParts FOREIGN KEY (officerID, userID, bLocation, dateDonated)
-        REFERENCES FinalProduct(officerID, userID, bLocation, dateDonated),
+    CONSTRAINT PK_fpParts PRIMARY KEY (rOfficerID, requestedBy, dateDonated, pSerialNumber),
+    CONSTRAINT FK_FinalProductParts FOREIGN KEY (rOfficerID, requestedBy, bLocation, dateDonated)
+        REFERENCES FinalProduct(rOfficerID, requestedBy, bLocation, dateDonated),
     CONSTRAINT FK_PartFinalPart FOREIGN KEY (pSerialNumber)
         REFERENCES Part(pSerialNumber)
 );
 
 CREATE TABLE IF NOT EXISTS FP_PartComp (
-    officerID       VARCHAR(36) NOT NULL,
-    userID          VARCHAR(36) NOT NULL,
+    rOfficerID      VARCHAR(36) NOT NULL,
+    requestedBy     VARCHAR(36) NOT NULL,
     bLocation       VARCHAR(50),
     dateDonated     DATE NOT NULL,
     cSerialNumber   VARCHAR(50) NOT NULL,
     pSerialNumber   VARCHAR(50) NOT NULL,
-    CONSTRAINT PK_fpPartComp PRIMARY KEY (officerID, userID, dateDonated, cSerialNumber, pSerialNumber),
-    CONSTRAINT FK_FinalProductPartComp FOREIGN KEY (officerID, userID, bLocation, dateDonated)
-        REFERENCES FinalProduct(officerID, userID, bLocation, dateDonated),
+    CONSTRAINT PK_fpPartComp PRIMARY KEY (rOfficerID, requestedBy, dateDonated, cSerialNumber, pSerialNumber),
+    CONSTRAINT FK_FinalProductPartComp FOREIGN KEY (rOfficerID, requestedBy, bLocation, dateDonated)
+        REFERENCES FinalProduct(rOfficerID, requestedBy, bLocation, dateDonated),
     CONSTRAINT FK_PartFinalPartComp FOREIGN KEY (pSerialNumber)
         REFERENCES Part(pSerialNumber),
     CONSTRAINT FK_CompFinalPartComp FOREIGN KEY (cSerialNumber)
@@ -192,14 +207,14 @@ CREATE TABLE IF NOT EXISTS FP_PartComp (
 );
 
 CREATE TABLE IF NOT EXISTS FP_Comp (
-    officerID       VARCHAR(36) NOT NULL,
-    userID          VARCHAR(36) NOT NULL,
+    rOfficerID      VARCHAR(36) NOT NULL,
+    requestedBy     VARCHAR(36) NOT NULL,
     bLocation       VARCHAR(50),
     dateDonated     DATE NOT NULL,
     cSerialNumber   VARCHAR(50) NOT NULL,
-    CONSTRAINT PK_fpPartComp PRIMARY KEY (officerID, userID, dateDonated, cSerialNumber),
-    CONSTRAINT FK_FinalProductComp FOREIGN KEY (officerID, userID, bLocation, dateDonated)
-        REFERENCES FinalProduct(officerID, userID, bLocation, dateDonated),
+    CONSTRAINT PK_fpPartComp PRIMARY KEY (rOfficerID, requestedBy, dateDonated, cSerialNumber),
+    CONSTRAINT FK_FinalProductComp FOREIGN KEY (rOfficerID, requestedBy, bLocation, dateDonated)
+        REFERENCES FinalProduct(rOfficerID, requestedBy, bLocation, dateDonated),
     CONSTRAINT FK_CompFinalComp FOREIGN KEY (cSerialNumber)
         REFERENCES Computer(cSerialNumber)
 );
