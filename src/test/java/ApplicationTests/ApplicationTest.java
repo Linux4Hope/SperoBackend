547d5f71efdae0b2768dc91468f0846c7ccd05c8
Narith <narithchoeun@hotmail.com>
2017-06-07 19:36:45 -0700

    rest assured working

--- src/test/java/ApplicationTests/ApplicationTest.java
+++ src/test/java/ApplicationTests/ApplicationTest.java
@@ -1,10 +1,15 @@
 package ApplicationTests;
 import app.Application;
+import io.restassured.RestAssured;
 import org.junit.*;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import org.hamcrest.Matchers.*;
+import io.restassured.module.jsv.JsonSchemaValidator.*;
 
 public class ApplicationTest {
     @Test
-    public void getHelloTest() {
+    public void numTest() {
         // Arrange
         Application app = new Application();
 
@@ -13,5 +18,18 @@ public class ApplicationTest {
 
         // Assert
         Assert.assertEquals(2, result);
+    }
+
+    @BeforeClass
+    public static void setup() {
+        RestAssured.port = Integer.valueOf(4567);
+        RestAssured.baseURI = "http://localhost";
+    }
+
+    @Test
+    public void getHelloRoute() {
+        Application app = new Application();
+        app.getHello();
+        get("/hello").then().assertThat().statusCode(200);
     }
 }
